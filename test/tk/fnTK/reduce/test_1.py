55d5f1ec3a28a70f654e3da26673b75ccd5fe75c
Yuhang(Steven) Wang <stevenaura@live.com>
2016-12-28 09:41:30 -0600

    move test/c to frontend

---
+++ test/tk/fnTK/reduce/test_1.py
@@ -0,0 +1,11 @@
+from ninjag.tk import fnTK
+from ninjag.tk import dictTK
+
+
+def test():
+    d1 = {"a": 1, "b": [1]}
+    d2 = {"a": 2, "b": [2]}
+    d3 = {"a": 3, "b": [3]}
+    answer = fnTK.reduce(dictTK.merge, [d1, d2, d3], {})
+    solution = {"a": 3, "b": [1, 2, 3]}
+    assert answer == solution
